500e38579808967d7b6e73d70bbe8251c0bf3c9f
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-08 23:36:27 +0200

    Update log: add 'warning' & started -> starting

--- src/lib/ragin-tasks/log.js
+++ src/lib/ragin-tasks/log.js
@@ -7,9 +7,12 @@ module.exports = {
     log(version.get(), 'green');
   },
   started: function(task) {
-    log('started ' + task, 'blue');
+    log('starting ' + task, 'blue');
   },
   finished: function(task) {
     log('finished ' + task, 'green');
+  },
+  warning: function(text, task) {
+    log(`task::${task}: ${text}`, 'yellow');
   }
 };
