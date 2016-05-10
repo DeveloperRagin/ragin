3b93dcefa72252b82fc302e0e22cca8739b3d61a
Glenn Vandeuren <vandeurenglenn@gmail.com>
2016-05-11 00:27:22 +0200

    Fix ESLint warinings

--- src/lib/plugin-tasks.js
+++ src/lib/plugin-tasks.js
@@ -3,13 +3,15 @@ var fs = require('fs');
 var raginTodo = require('./plugin-tasks/ragin-todo');
 var getDependencies = require('./get-dependencies');
 
-module.exports = (plugin) => {
-  var bowerFile = fs.readFileSync(plugin.__path__.replace(plugin.name + '.html', 'bower.json'));
+module.exports = plugin => {
+  var path = plugin.__path__.replace(plugin.name + '.html', 'bower.json');
+  var bowerFile = fs.readFileSync(path);
   switch (plugin.name) {
     case 'ragin-todo':
       raginTodo();
       break;
     default:
   }
-  getDependencies(plugin.__componentsPath__ + '/', JSON.parse(bowerFile).dependencies);
+  var dependencies = JSON.parse(bowerFile).dependencies;
+  getDependencies(plugin.__componentsPath__ + '/', dependencies);
 };
